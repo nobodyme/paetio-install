c5765d8a37cb2e1374796b5a25cca99ba14372b0
nobodyme <naveenkumarspa@gmail.com>
2018-07-05 20:31:30 +0530

    rails installation bugs

--- 3-rails-sql.sh
+++ 3-rails-sql.sh
@@ -1,17 +1,19 @@
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
-
-rbenv install 2.2.8 --verbose
-rbenv global 2.2.8
-
-echo "gem: --no-ri --no-rdoc" > ~/.gemrc
-gem install bundler
-rbenv rehash
-
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 apt-get install -y nodejs
 
-gem install rails -v 5.2.0
-rbenv rehash
-
 apt-get install -y mysql-server mysql-client libmysqlclient-dev
+
+
+echo "open and install ruby and rails according to the commands in this file"
+
+# export LC_ALL=en_US.UTF-8
+# export LANG=en_US.UTF-8
+
+# #rbenv install 2.2.8 --verbose
+# #rbenv global 2.2.8
+
+# #echo "gem: --no-ri --no-rdoc" > ~/.gemrc
+# #gem install bundler
+# #rbenv rehash
+# #gem install rails -v 5.2.0
+# #rbenv rehash
