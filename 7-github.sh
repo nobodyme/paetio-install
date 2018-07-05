14043f0339219a419518a6326126bb36ab7d09a3
nobodyme <naveenkumarspa@gmail.com>
2018-07-05 16:46:44 +0530

    file names changed & comment enhanced

---
+++ 7-github.sh
@@ -0,0 +1,15 @@
+git clone https://devappoets@bitbucket.org/appoets/peatio.git
+cd ~/peatio
+bundle install
+# on error do json update and do bundle install again like the following
+bundle update json
+bundle install
+
+# edit config/application.yml to set up site URL_HOST
+# paste same bitcoin username and password in config/currenices.yml
+
+# edit seeds.rb file to input admin user name and password for the website
+# bundle exec rake db:setup
+
+# bundle exec rake daemons:start
+# bundle exec rails server
